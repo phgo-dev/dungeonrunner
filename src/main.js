9d6a69cf611870deea82d2cf5d81b828c6e75d2c
phgo-dev <p-goetz@gmx.net>
2022-03-07 21:36:56 +0100

    add "fog of war", hide content for non start tiles

--- src/main.js
+++ src/main.js
@@ -11,10 +11,17 @@ function createMap(){
 
 function createColumn(Tile, startTile, targetTile){
     var tileType = 'btn-light';
-    if(Tile.x == startTile[0] && Tile.y == startTile[1]) tileType = 'btn-primary';
-    if(Tile.x == targetTile[0] && Tile.y == targetTile[1]) tileType = 'btn-info';
+    var isStartTile = Tile.x == startTile[0] && Tile.y == startTile[1];
+    var isTargetTile = Tile.x == targetTile[0] && Tile.y == targetTile[1];
     var margin = Math.abs(Tile.size-3);
-    return '<button type="button" class="btn ' + tileType + ' m-' + margin + ' p-' + Tile.size + '">' + Tile.countConnections +  '|' + Tile.size + ' ' + '</button>';
+    //normal tiles, no text
+    if(isTargetTile) tileType = 'btn-info';
+    if(!isStartTile) {
+        return '<button type="button" class="btn ' + tileType + ' m-' + margin + ' p-' + Tile.size + '">&nbsp;&nbsp;&nbsp;</button>';
+    }
+    //only for start tile show content
+    if(isStartTile) tileType = 'btn-primary';
+    return '<button type="button" class="btn ' + tileType + ' m-' + margin + ' p-' + Tile.size + '">You</button>';
 }
 
 function drawTile(tileColumn, startTile, targetTile){
